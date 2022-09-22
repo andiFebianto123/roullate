f44d810b3debca91c4d268584dd4bf9ad7605c5c
andiFebianto123 <andifebianto123@gmail.com>
2022-09-22 10:27:14 +0700

    content front end fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Spin2Win Wheel</title>
+    <title>RouLAtte</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- <link href='https://fonts.googleapis.com/css?family=Fjalla+One' rel='stylesheet' type='text/css'> -->
     <!-- <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}"> -->
